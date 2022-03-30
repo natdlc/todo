4ca9174ae0e015a817d10663b85478bf5d438939
Nat Corpuz <nate.corpuz1@gmail.com>
2022-03-30 17:40:51 +0800

    reset

---
+++ src/dom.js
@@ -0,0 +1,8 @@
+import calendarIcon from './images/calendar.svg';
+import addIcon from './images/add.svg';
+
+const insertStructure = () => {
+
+}
+
+export {insertStructure}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import './reset.css';
 import './style.css';
-import * as DOM from './dom-inserts';
+import * as DOM from './dom.js';
 
 (function () {
-    const DOMElements = DOM.insertStructure();
 
-    const addSectionBtnClick = () => {
-        const addSectionModal = DOM.addSection(DOMElements.main);
-    };
-
-    DOMElements.headerAddSectionBtn.addEventListener('click', addSectionBtnClick);
-    
 })();
