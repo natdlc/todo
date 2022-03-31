084978bc8b5f5d1780e6321d1495a1b71f9d0463
Nat Corpuz <nate.corpuz1@gmail.com>
2022-03-31 08:34:04 +0800

    append complete

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import {insertHeader, insertMain} from './dom/dom.js';
 import './styles/reset.css';
 import './styles/style.css';
 
-    console.log(insertHeader);
-    insertHeader();
-    insertMain();
+insertHeader();
+insertMain();
 
-    //organize dom structure and styles
-        //start with appending html structure
-        //follow solid principles
-        //then organize styles
-    
-    //create mock logic through class / factory function
+//organize dom structure and styles
+    //start with appending html structure
+    //follow solid principles
+    //then organize styles
+
+//create mock logic through class / factory function
