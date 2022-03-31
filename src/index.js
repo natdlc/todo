193a4272659cb004cd7398a4eef311fa70499263
Nat Corpuz <nate.corpuz1@gmail.com>
2022-03-31 09:01:34 +0800

    edit comments

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import {insertHeader, insertMain} from './dom/dom.js';
 import './styles/reset.css';
 import './styles/style.css';
 
-insertHeader();
-insertMain();
+(function (){
+    insertHeader();
+    insertMain();
+})();
 
 //organize dom structure and styles
-    //then organize styles
+    //plan and create a new branch any future changes
+    //organize styles
 
 //create mock logic through class / factory function
+
