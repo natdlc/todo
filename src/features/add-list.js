3147404ad083b391c28493d1668596cbeff0772d
nat corpuz <nate.corpuz1@gmail.com>
2022-04-06 19:51:10 +0800

    populate dom and update storage wip

--- src/features/add-list.js
+++ src/features/add-list.js
@@ -1,26 +1,30 @@
 import {insertList} from '../dom/main/1-lists-wrapper/10-list.js';
+//create container for list objects
 //when user clicks add list
-    //create a new list object
-    //populate list object with title
+    //create new list object
+    //push new list object to container
 
 class List {
     constructor(title) {
         this.title = title;
     }
 }
-    
+
 const addListBtn = () => {
     const btn = document.querySelector('.add-list-btn');
     const listsWrapper = document.querySelector('.lists-wrapper');
     btn.addEventListener('click', () => {
         insertList(listsWrapper)
-        // addListToStorage();
+        addListToStorage();
     });
 }
 
 const addListToStorage = () => {
     const date = document.querySelector('input[type=date]');
-    localStorage.getItem(date.value);
+    const newList = new List('');
+    const listsArray = JSON.parse(localStorage.getItem(date.value));
+    listsArray.push(newList);
+    localStorage.setItem(date.value, JSON.stringify(listsArray));
     console.log(localStorage);
 }
 
--- src/features/date-input.js
+++ src/features/date-input.js
@@ -1,8 +1,39 @@
+import { insertList } from "../dom/main/1-lists-wrapper/10-list";
+
 const onDateChange = e => {
     const dateSelected = e.target.value;
+    
     //if storage doesnt have a value for the date yet
         //set date selected as key and initialize 
-    console.log(localStorage);
+    if (!localStorage.getItem(dateSelected)) {
+        const listsArray = [];
+        localStorage.setItem(dateSelected, JSON.stringify(listsArray));
+        console.log('ran');
+    }
+
+    const listsArr = JSON.parse(localStorage.getItem(dateSelected));
+
+    for (let i = 0; i < listsArr.length; i++) {
+        const listsWrapper = document.querySelector('.lists-wrapper');
+        //insert list to dom
+        insertList(listsWrapper);
+
+        //retrieve lists container from localstorage
+        const listsArray = JSON.parse(localStorage.getItem(dateSelected));
+        
+        //select individual list from array
+        const currentList = listsArray[i];
+
+        //select list from dom
+        const listsDom = document.querySelectorAll('.list');
+        const listDom = listsDom[i];
+        
+        //select list's title from dom
+        const listTitle = listDom.children[0].children[1];
+        
+        //set individual list's title text to the one from localstorage
+        listTitle.innerText = currentList.title;
+    }
 }
 
 export {onDateChange};
