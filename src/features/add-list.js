366d2f8a097db47ad208552fb31868a164044ac7
nat corpuz <nate.corpuz1@gmail.com>
2022-04-07 16:28:45 +0800

    wip

--- src/features/add-list.js
+++ src/features/add-list.js
@@ -1,8 +1,5 @@
 import {insertList} from '../dom/main/1-lists-wrapper/10-list.js';
-import {updateStorage} from './edit-list.js';
-
-//BUG: Add list does not have a link to local storage
-//BUG: list titles added from addlistbtn does not have a link to local storage
+import { setListTitles } from './edit-list.js';
 
 class List {
     constructor(title) {
@@ -16,14 +13,13 @@ const addListBtn = () => {
     btn.addEventListener('click', e => {
         insertList(listsWrapper);
         addListToStorage();
-        const dateSelected = document.querySelector('input[type=date]').value;
-        const listHtmlCollection = e.path[2].children[1].children;
-        const addedList = listHtmlCollection[listHtmlCollection.length - 1];
-        const listTitle = addedList.children[0].children[1];
-        listTitle.addEventListener('input', e => {
-            console.log(e.target.innerText);
-        })
 
+        const dateSelected = document.querySelector('input[type=date]').value;
+        const listsArr = JSON.parse(localStorage.getItem(dateSelected));
+        // loop through all list items
+        // attach event listener to each h1
+        // use iterator to connect and update h1 content against storage array
+        console.log(e.path[2].children[1].children);
     });
 }
 
--- src/features/edit-list.js
+++ src/features/edit-list.js
@@ -3,23 +3,18 @@ import { insertList } from "../dom/main/1-lists-wrapper/10-list";
 const setListTitles = (listsArr, dateSelected) => {
     for (let i = 0; i < listsArr.length; i++) {
         const listsWrapper = document.querySelector('.lists-wrapper');
-        //insert list to dom
+
         insertList(listsWrapper);
 
-        //retrieve lists container from localstorage
         const listsArray = JSON.parse(localStorage.getItem(dateSelected));
         
-        //select individual list from array
         const currentList = listsArray[i];
 
-        //select list from dom
         const listsDom = document.querySelectorAll('.list');
         const listDom = listsDom[i];
         
-        //select list's title from dom
         const listTitle = listDom.children[0].children[1];
         
-        //set individual list's title text to the one from localstorage
         listTitle.innerText = currentList.title;
 
         listTitle.addEventListener('input', e => {updateStorage(i, dateSelected, e)})
