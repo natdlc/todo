d041efa801a7219307f1bd8f70e9a4dad8db6ed6
nat corpuz <nate.corpuz1@gmail.com>
2022-04-08 19:36:17 +0800

    reset

--- src/features/del-list.js
+++ src/features/del-list.js
@@ -1,13 +1,5 @@
 const listDelBtnEvent = e => {
-    const dateSelected = document.querySelector('input[type=date]').value;
-    const title = e.path[1].children[1].innerText;
-    const list = e.path[2];
-    list.remove();
-    const listsArray = JSON.parse(localStorage.getItem(dateSelected))
-    const matchesDomTitle = obj => obj.title == title;
-    const index = listsArray.findIndex(matchesDomTitle);
-    listsArray.splice(index, 1);
-    localStorage.setItem(dateSelected, JSON.stringify(listsArray));
+    console.log(e);
 };
 
 export {listDelBtnEvent};
