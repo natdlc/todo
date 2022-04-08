994cc65c6a5193f1940b7b1418a32ddfd5dbae15
nat corpuz <nate.corpuz1@gmail.com>
2022-04-09 05:14:06 +0800

    list delete from dom and storage

--- src/features/del-list.js
+++ src/features/del-list.js
@@ -1,5 +1,30 @@
 const listDelBtnEvent = e => {
-    console.log(e);
+    // get clicked list element
+    const clickedList = e.path[2];
+
+    // get list wrapper nodelist
+    const listNodeList = document.querySelector('.lists-wrapper').childNodes;
+
+    // make an array from nodelist
+    const listNodeListArray = Array.from(listNodeList);
+
+    // get date selected
+    const dateSelected = document.querySelector('input[type=date]').value;
+
+    // get index of clicked list from nodelist array
+    const listDomIndex = listNodeListArray.indexOf(clickedList);
+
+    // get local storage list array (converted)
+    const storageListArray = JSON.parse(localStorage.getItem(dateSelected));
+
+    // splice list from local storage list array
+    storageListArray.splice(listDomIndex, 1);
+
+    // set local storage list array with new spliced list array
+    localStorage.setItem(dateSelected, JSON.stringify(storageListArray));
+
+    // remove clicked list from dom
+    clickedList.remove();
 };
 
 export {listDelBtnEvent};
